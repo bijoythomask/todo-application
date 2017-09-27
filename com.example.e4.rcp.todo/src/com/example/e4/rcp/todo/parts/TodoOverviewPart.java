683d29e57268fbe2e9e77b551144828303b4ab39
Bijoy Thomas <bijoythomask@yahoo.com>
2017-09-27 11:46:34 +0530

    Created a fragment which contribute to the perspective

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
@@ -2,6 +2,12 @@
 package com.example.e4.rcp.todo.parts;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.widgets.Composite;
 
 public class TodoOverviewPart {
@@ -9,8 +15,15 @@ public class TodoOverviewPart {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		
-		System.out.println(this.getClass().getSimpleName() + " @PostConstruct method called.");
-		
+	}
+	
+	@Inject
+	@Optional
+	public void receiveActivePart(
+	    @Named(IServiceConstants.ACTIVE_PART) MPart activePart) {
+	    if (activePart != null) {
+	        System.out.println("Active part changed " + activePart.getLabel());
+	    }
 	}
 
 }
